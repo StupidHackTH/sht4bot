2fa710b1b7e5d6854e739446c3a75a94147d9b83
Glitch (hello-express) <none>
2020-06-23 15:29:33 +0000

    💴👲 Checkpoint ./handlers/message.js:577339/7508

--- handlers/message.js
+++ handlers/message.js
@@ -22,6 +22,12 @@ module.exports = async function onMessage(message, { client, firebase, storeRef 
   
   // Direct messages
   if (!message.guild) {
+    const member = guild.members.resolve(message.author.id)
+    if (!member) {
+      message.reply('You are not in the hackathon discord server. Please join first!')
+      return
+    }
+
     if (message.content.startsWith(';')) {
       if (!adminIds.includes(message.author.id)) {
         message.reply('Unauthorized')
@@ -75,16 +81,75 @@ module.exports = async function onMessage(message, { client, firebase, storeRef 
     // New team
     if (text.toLowerCase() === 'new team') {
       const teamRoles = [...guild.roles.cache.values()].filter(r => /^team/.test(r.name))
-      const 
       const vacantTeamRoles = teamRoles.filter(r => r.members.size === 0).sort(() => Math.random() - 0.5)
+      const alreadyInTeamRoles = teamRoles.filter(r => r.members.has(message.author.id))
+      if (alreadyInTeamRoles.length > 0) {
+        message.reply('You already have a team. To leave your current team, say "leave team".')
+        return
+      }
       if (vacantTeamRoles.length === 0) {
         message.reply('Sorry, we have a maximum limit of 24 teams. Please join join an existing team or contact organizers for help.')
         return
       }
       const role = vacantTeamRoles[0]
-      message.reply(role.name)
+      await member.roles.add(role)
+      const joinKey = String(100000 + Math.floor(Math.random() * 900000))
+      const joinKeyRef = storeRef.child('teams').child(role.name).child('joinKey')
+      await joinKeyRef.set(joinKey)
+      message.reply(`You are now on **${role.name}**. Other people can join your team by saying: "join ${role.name} ${joinKey}"`)
+      return
     }
 
+    // Leave team
+    if (text.toLowerCase() === 'leave team') {
+      const teamRoles = [...guild.roles.cache.values()].filter(r => /^team/.test(r.name))
+      const alreadyInTeamRoles = teamRoles.filter(r => r.members.has(message.author.id))
+      if (alreadyInTeamRoles.length > 0) {
+        for (const role of alreadyInTeamRoles) {
+          await member.roles.remove(role)
+          message.reply(`You left **${role.name}**.`)
+        }
+        return
+      } else {
+        message.reply(`You are not in a team. You must be inside a team in order to leave it.`)
+        return
+      }
+    }
+    
+    // Join team
+    {
+      const match = text.match(/^join (team\d+) (\d+)$/i)
+      if (match) {
+        const teamRoles = [...guild.roles.cache.values()].filter(r => /^team/.test(r.name))
+        const alreadyInTeamRoles = teamRoles.filter(r => r.members.has(message.author.id))
+        if (alreadyInTeamRoles.length > 0) {
+          message.reply('You already have a team. To leave your current team, say "leave team".')
+          return
+        }
+        const teamName = match[1].toLowerCase()
+        const role = teamRoles.find(r => r.name === teamName)
+        if (!role) {
+          message.reply(`Sorry, ${teamName} not found.`)
+          return
+        }
+        const joinKeyRef = storeRef.child('teams').child(role.name).child('joinKey')
+        const joinKey = (await joinKeyRef.once('value')).val()
+        if (match[2] === joinKey) {
+          await member.roles.add(role)
+          message.reply(`You are now on **${role.name}**. Other people can join your team by saying: "join ${role.name} ${joinKey}"`)
+        } else {
+          message.reply(`Sorry, join passcode is incorrect. Please make sure you have the correct join passcode, and try again. Please contact the organizers if you need help.`)
+        }
+        return
+      }
+    }
+    
+    if (text === 'help') {
+      message.reply(`To write later............`)
+      return
+    }
+
+    message.reply(`Unknown command. Say "help" for help.`)
     return
   } // End direct messages
 
